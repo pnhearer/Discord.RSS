ac2fc7198e64dd261524216d4686e51937691551
synzen <michael994664@gmail.com>
2019-01-01 23:06:03 -0500

    Remove web files erroneously placed on dev

--- web/client/src/js/actions/index-actions.js
+++
@@ -1,5 +0,0 @@
-import { TEST_ACTION } from '../constants/action-types.js'
-
-export function testAction (payload) {
-  return { type: TEST_ACTION, payload }
-}
--- web/client/src/js/constants/action-types.js
+++
@@ -1,2 +0,0 @@
-export const UPDATE_GUILD = 'UPDATE_GUILD'
-export const TEST_ACTION = 'TEST_ACTION'
